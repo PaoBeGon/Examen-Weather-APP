32512ae4fbc0a17863e0c0c6590e388ebc0b6000
Paola Maristell Begazo Gonzales <paobegon1@gmail.com>
2016-09-02 18:03:31 -0500

    falta swipe y mostrar otra ciudad

--- jsw/main.js
+++ jsw/main.js
@@ -12,16 +12,6 @@ function getWeather(id,res) {
 }
 getWeather("weather",dis);
 getWeather("temp",temp);
-///
-
-var weather2 = new XMLHttpRequest();
-weather2.open("GET", "http://api.wunderground.com/api/bd0893ab7e76c5ac/conditions/q/CA/San_Francisco.json", false);
-weather2.send(null);
-
-var r = JSON.parse(weather.response);
-
-var dis = "Lugar: " + r.current_observation.display_location.full + "  <p>";
-var temp = r.current_observation.temperature_string+ "  <p>";
 
 function getWeather(id,res) {
   document.getElementById(id).innerHTML=res;
