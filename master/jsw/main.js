3829cd6df5397668fa3cca67bcfb4177d340270b
Paola Maristell Begazo Gonzales <paobegon1@gmail.com>
2016-09-05 14:59:15 -0500

    mmm

---
+++ master/jsw/main.js
@@ -0,0 +1,20 @@
+var weather = new XMLHttpRequest();
+weather.open("GET", "http://api.wunderground.com/api/fa5715ba5adff2ac/conditions/q/CA/Los_angeles.json", false);
+weather.send(null);
+
+var r = JSON.parse(weather.response);
+
+var dis = "Lugar: " + r.current_observation.display_location.full + "  <p>";
+var temp = r.current_observation.temperature_string+ "  <p>";
+
+function getWeather(id,res) {
+  document.getElementById(id).innerHTML=res;
+}
+getWeather("weather",dis);
+getWeather("temp",temp);
+
+function getWeather(id,res) {
+  document.getElementById(id).innerHTML=res;
+}
+getWeather("weather2",dis);
+getWeather("temp2",temp);
